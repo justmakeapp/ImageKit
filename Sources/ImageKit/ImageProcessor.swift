236d9e5729415bb059aed52f4608b1dfc5e86c84
Long Vu <long.vudai01@gmail.com>
2024-03-13 14:13:07 +0700

    add megapixels

--- Sources/ImageKit/ImageProcessor.swift
+++ Sources/ImageKit/ImageProcessor.swift
@@ -18,6 +18,36 @@ public final class ImageProcessor: SynchronousImageProcessing {
     private let image: ImageSource
     private(set) var source: CGImageSource?
 
+    private(set) var _properties: [String: Any]?
+    public private(set) var properties: [String: Any]? {
+        get {
+            if let _properties {
+                return _properties
+            } else {
+                guard
+                    let source,
+                    /// lấy image đầu tiền, vì JPEG có thể embed cả thumbnail vào
+                    let firstIndexProperties = CGImageSourceCopyPropertiesAtIndex(source, 0, nil) as? [String: Any]
+                else {
+                    return nil
+                }
+                _properties = firstIndexProperties
+                return firstIndexProperties
+            }
+        }
+        set {
+            _properties = newValue
+        }
+    }
+
+    public var tiffDictionary: [String: Any]? {
+        properties?[kCGImagePropertyTIFFDictionary as String] as? [String: Any]
+    }
+
+    public var exifDictionary: [String: Any]? {
+        properties?[kCGImagePropertyExifDictionary as String] as? [String: Any]
+    }
+
     public init(image: ImageSource) {
         self.image = image
         initializeImageSource()
@@ -42,12 +72,6 @@ public final class ImageProcessor: SynchronousImageProcessing {
         guard let source else {
             return nil
         }
-//        let properties = CGImageSourceCopyProperties(source, nil) as? [String: Any]
-
-        /// lấy image đầu tiền, vì JPEG có thể embed cả thumbnail vào
-        guard let firstIndexProperties = CGImageSourceCopyPropertiesAtIndex(source, 0, nil) as? [String: Any] else {
-            return nil
-        }
 
         let width: Int = {
             var result: Int?
@@ -55,7 +79,7 @@ public final class ImageProcessor: SynchronousImageProcessing {
 //            if let properties, let width = properties[propertyKey] {
 //                return width as? CGFloat
 //            }
-            if let width = firstIndexProperties[propertyKey] {
+            if let width = properties?[propertyKey] {
                 result = width as? Int
             }
             return result ?? 0
@@ -68,7 +92,7 @@ public final class ImageProcessor: SynchronousImageProcessing {
 //            if let properties, let height = properties[propertyKey] {
 //                return height as? CGFloat
 //            }
-            if let height = firstIndexProperties[propertyKey] {
+            if let height = properties?[propertyKey] {
                 result = height as? Int
             }
 
@@ -76,7 +100,7 @@ public final class ImageProcessor: SynchronousImageProcessing {
         }()
 
         let orientationKey = kCGImagePropertyOrientation as String
-        let orientation = (firstIndexProperties[orientationKey] as? Int) ??
+        let orientation = (properties?[orientationKey] as? Int) ??
             1 // Default to 1 if orientation is not specified
 
         var newWidth = width
@@ -93,6 +117,21 @@ public final class ImageProcessor: SynchronousImageProcessing {
         }
 
         return CGSize(width: newWidth, height: newHeight)
+    }
+
+    public var megapixels: Double? {
+        // Image DPI for calculating megapixels
+        let dpiWidth = properties?[kCGImagePropertyDPIWidth as String] as? CGFloat ?? 72
+        let dpiHeight = properties?[kCGImagePropertyDPIHeight as String] as? CGFloat ?? 72
+
+        // Calculate megapixels
+        if let pixelWidth = properties?[kCGImagePropertyPixelWidth as String] as? CGFloat,
+           let pixelHeight = properties?[kCGImagePropertyPixelHeight as String] as? CGFloat {
+            let megapixels = (pixelWidth * dpiWidth / 72.0) * (pixelHeight * dpiHeight / 72.0) / 1_000_000.0
+            return megapixels
+        } else {
+            return nil
+        }
     }
 
     public func getImageFileSize() -> Double? {
